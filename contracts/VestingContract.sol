3ccee795ea7961fbec472820703655b2df31b4eb
Niall Moore <niall@watr.org>
2023-02-21 13:45:00 +0000

    feat(vesting): add basic contract prototype

---
+++ contracts/VestingContract.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract TokenVesting is Ownable, ReentrancyGuard {
+    address payee;
+    uint256 genesis_timestamp;
+    uint256 epoch_length;
+    uint256 payout;
+    uint8 number_of_epochs;
+    constructor(address _payee, uint64 _epoch_length, uint8 _number_of_epochs, uint256 allocation, uint256 _genesis_timestamp) {
+        payee = _payee;
+        epoch_length = _epoch_length;
+        payout = allocation/_number_of_epochs;
+        number_of_epochs = _number_of_epochs;
+        genesis_timestamp = _genesis_timestamp;
+    }
+
+    receive() external payable {}
+
+    function withdraw(address payable _to) nonReentrant public {
+        require(_to == payee, "Cannot withdraw to this address");
+        uint256 withdrawable = checkWithdrawable();
+        require(withdrawable > 0, "Nothing to withdraw");
+        (bool sent, ) = _to.call{value: withdrawable}("");
+        require(sent, "failed to withdraw funds");
+    }
+
+    function checkWithdrawable() view private returns (uint256) {
+        uint256 balance = address(this).balance;
+        return balance - locked();
+    }
+
+    function locked() view public returns (uint256) {
+        uint256 current_time = block.timestamp;
+        uint256 interval = current_time - genesis_timestamp;
+        uint256 epochs =number_of_epochs - Math.min(interval/epoch_length, number_of_epochs);
+        return payout * epochs;
+    } 
+}
---
+++ test/VestingContract.test.js
@@ -0,0 +1,51 @@
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
+const chai = require("chai")
+const { solidity } = require("ethereum-waffle");
+
+chai.use(solidity);
+
+describe("VestingContract", () => {
+    it("should return locked balance", async () => {
+        const factory = await ethers.getContractFactory('TokenVesting')
+        const now = (await ethers.provider.getBlock("latest")).timestamp
+        // address _payee, uint64 _epoch_length, uint8 _number_of_epochs, uint256 allocation
+        const contract = await factory.deploy("0x98f457EDcd11dB513cBf631a759FD94E64066323", 3600, 4, "1000000000000000000", now)
+        await contract.deployed()
+        chai.expect(await contract.locked() == "1000000000000000000")
+    }),
+    it("should unlock as time progresses", async () => {
+        const factory = await ethers.getContractFactory('TokenVesting')
+        const now = (await ethers.provider.getBlock("latest")).timestamp
+        // address _payee, uint64 _epoch_length, uint8 _number_of_epochs, uint256 allocation
+        const contract = await factory.deploy("0x98f457EDcd11dB513cBf631a759FD94E64066323", 3600, 4, "1000000000000000000", now)
+        await contract.deployed()
+        chai.expect(await contract.locked() == "1000000000000000000")
+        await time.increase(3601)
+        chai.expect(await contract.locked() == "750000000000000000")
+        await time.increase(3601)
+        chai.expect(await contract.locked() == "500000000000000000")
+        await time.increase(3601)
+        chai.expect(await contract.locked() == "250000000000000000")
+        await time.increase(720000)
+        chai.expect(await contract.locked() == "0")
+    }),
+    it("should allow withdrawal after unlocks", async () => {
+        const factory = await ethers.getContractFactory('TokenVesting')
+        const now = (await ethers.provider.getBlock("latest")).timestamp
+        // address _payee, uint64 _epoch_length, uint8 _number_of_epochs, uint256 allocation
+        const contract = await factory.deploy("0x98f457EDcd11dB513cBf631a759FD94E64066323", 3600, 4, "1000000000000000000", now)
+        await contract.deployed()
+        const [owner, ] = await ethers.getSigners();
+        await owner.sendTransaction({
+            to: contract.address,
+            value: ethers.utils.parseEther("1.0"), // Sends exactly 1.0 ether
+          });
+        chai.expect(await ethers.provider.getBalance(contract.address), "1000000000000000000")
+        await time.increase(3601)
+        await contract.withdraw("0x98f457EDcd11dB513cBf631a759FD94E64066323")
+        const contractBalance = await ethers.provider.getBalance(contract.address)
+        chai.expect(contractBalance).to.equal("750000000000000000")
+        const payeeBalance = await ethers.provider.getBalance("0x98f457EDcd11dB513cBf631a759FD94E64066323")
+        chai.expect(payeeBalance).to.equal("250000000000000000")
+    })
+})
