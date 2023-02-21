3366614cbfe51f4fe8c447d3af44da9ff24e6653
Niall Moore <niall@watr.org>
2023-02-21 18:55:09 +0000

    test(payee): improve resilience by using random wallet for each test

--- test/VestingContract.test.js
+++ test/VestingContract.test.js
@@ -6,18 +6,20 @@ chai.use(solidity);
 
 describe("VestingContract", () => {
     it("should return locked balance", async () => {
+        const payeeWallet = await ethers.Wallet.createRandom()
         const factory = await ethers.getContractFactory('TokenVesting')
         const now = (await ethers.provider.getBlock("latest")).timestamp
         // address _payee, uint64 _epoch_length, uint8 _number_of_epochs, uint256 allocation
-        const contract = await factory.deploy("0x98f457EDcd11dB513cBf631a759FD94E64066323", 3600, 4, "1000000000000000000", now)
+        const contract = await factory.deploy(payeeWallet.address, 3600, 4, "1000000000000000000", now)
         await contract.deployed()
         chai.expect(await contract.locked() == "1000000000000000000")
     }),
     it("should unlock as time progresses", async () => {
+        const payeeWallet = await ethers.Wallet.createRandom()
         const factory = await ethers.getContractFactory('TokenVesting')
         const now = (await ethers.provider.getBlock("latest")).timestamp
         // address _payee, uint64 _epoch_length, uint8 _number_of_epochs, uint256 allocation
-        const contract = await factory.deploy("0x98f457EDcd11dB513cBf631a759FD94E64066323", 3600, 4, "1000000000000000000", now)
+        const contract = await factory.deploy(payeeWallet.address, 3600, 4, "1000000000000000000", now)
         await contract.deployed()
         chai.expect(await contract.locked() == "1000000000000000000")
         await time.increase(3601)
@@ -30,10 +32,11 @@ describe("VestingContract", () => {
         chai.expect(await contract.locked() == "0")
     }),
     it("should allow withdrawal after unlocks", async () => {
+        const payeeWallet = await ethers.Wallet.createRandom()
         const factory = await ethers.getContractFactory('TokenVesting')
         const now = (await ethers.provider.getBlock("latest")).timestamp
         // address _payee, uint64 _epoch_length, uint8 _number_of_epochs, uint256 allocation
-        const contract = await factory.deploy("0x98f457EDcd11dB513cBf631a759FD94E64066323", 3600, 4, "1000000000000000000", now)
+        const contract = await factory.deploy(payeeWallet.address, 3600, 4, "1000000000000000000", now)
         await contract.deployed()
         const [owner, ] = await ethers.getSigners();
         await owner.sendTransaction({
@@ -42,10 +45,10 @@ describe("VestingContract", () => {
           });
         chai.expect(await ethers.provider.getBalance(contract.address), "1000000000000000000")
         await time.increase(3601)
-        await contract.withdraw("0x98f457EDcd11dB513cBf631a759FD94E64066323")
+        await contract.withdraw(payeeWallet.address)
         const contractBalance = await ethers.provider.getBalance(contract.address)
         chai.expect(contractBalance).to.equal("750000000000000000")
-        const payeeBalance = await ethers.provider.getBalance("0x98f457EDcd11dB513cBf631a759FD94E64066323")
+        const payeeBalance = await ethers.provider.getBalance(payeeWallet.address)
         chai.expect(payeeBalance).to.equal("250000000000000000")
     })
 })
