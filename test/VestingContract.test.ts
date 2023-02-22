14a8a44a92988558b9897faaa026e1174a629807
Niall Moore <niall@watr.org>
2023-02-22 10:42:49 +0000

    chore(types): migrate js -> ts

--- test/VestingContract.test.ts
+++ test/VestingContract.test.ts
@@ -1,5 +1,8 @@
-const { time } = require("@nomicfoundation/hardhat-network-helpers");
-const { expect } = require("chai")
+import { time } from "@nomicfoundation/hardhat-network-helpers"
+import hre from "hardhat";
+import { expect } from "chai"
+
+const ethers = hre.ethers
 
 describe("VestingContract", () => {
     it("should return locked balance", async () => {
@@ -65,5 +68,20 @@ describe("VestingContract", () => {
         expect(await contract.locked() == "750000000000000000")
         await expect(contract.withdraw(nonPayeeWallet.address)).to.be.revertedWith("Cannot withdraw to this address")
         
+    }),
+    it("should not allow withdrawal if there is nothing to withdraw", async() => {
+        const payeeWallet = await ethers.Wallet.createRandom()
+        const factory = await ethers.getContractFactory('TokenVesting')
+        const now = (await ethers.provider.getBlock("latest")).timestamp
+        // address _payee, uint64 _epoch_length, uint8 _number_of_epochs, uint256 allocation
+        const contract = await factory.deploy(payeeWallet.address, 3600, 4, "1000000000000000000", now)
+        await contract.deployed()
+        const [owner, ] = await ethers.getSigners();
+        await owner.sendTransaction({
+            to: contract.address,
+            value: ethers.utils.parseEther("1.0"), // Sends exactly 1.0 ether
+          });
+        expect(await ethers.provider.getBalance(contract.address), "1000000000000000000")
+        await expect(contract.withdraw(payeeWallet.address)).to.be.revertedWith("Nothing to withdraw")
     })
 })
